5ff054c85df86022bc6eff7611d1d26722ae82ee
Földi Bianka Johanna <63818487+foldibianka15@users.noreply.github.com>
2023-12-04 22:33:15 +0100

    MapActivity created

--- app/src/main/java/com/example/projektmunka/MapActivity.kt
+++ app/src/main/java/com/example/projektmunka/MapActivity.kt
@@ -1,50 +1,118 @@
 package com.example.projektmunka
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.projektmunka.databinding.ActivityMapBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import org.osmdroid.api.IMapController
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 class MapActivity : AppCompatActivity() {
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
     private lateinit var binding: ActivityMapBinding
+    private lateinit var mMap: MapView
+    lateinit var controller: IMapController
+    private lateinit var locationManager: LocationManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_map)
 
-        // Set up the bottom sheet
-        /*val bottomSheet: FrameLayout = findViewById(R.id.bottom_sheet)
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN*/
+        displayMap()
 
-        BottomSheetBehavior.from(findViewById(R.id.bottom_sheet)).apply {
-            peekHeight=100
-            this.state=BottomSheetBehavior.STATE_COLLAPSED
+        // Set up the bottom sheet
+        val bottomSheet: FrameLayout = findViewById(R.id.bottom_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.apply {
+            peekHeight = 100
+            this.state = BottomSheetBehavior.STATE_HIDDEN
         }
 
         val data: Int = intent.getIntExtra("key", 0)
         onMapItemSelected(data)
-
     }
 
-    // Method to show the bottom sheet
+    fun displayMap() {
+        //Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        Configuration.getInstance().load(
+            applicationContext,
+            getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE)
+        )
+
+        mMap = binding.mMap
+        mMap.apply {
+            setTileSource(TileSourceFactory.MAPNIK)
+            isHorizontalMapRepetitionEnabled = false
+            isVerticalMapRepetitionEnabled = false
+            setMultiTouchControls(true)
+            setBuiltInZoomControls(true)
+        }
+
+        controller = mMap.controller
+        controller.setZoom(15.0)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+// Check for location permissions
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request location permissions if not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+        } else {
+            // Request location updates
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
+        }
+    }
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            // Update the map center to the new location
+            val newLocation = GeoPoint(location.latitude, location.longitude)
+            mMap.controller.setCenter(newLocation)
+        }
+    }
+
     fun showBottomSheet() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
-    // Method to hide the bottom sheet
     fun hideBottomSheet() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
-
 
     private fun inflateFormLayout(formType: Int): View {
         val inflater = LayoutInflater.from(this)
@@ -57,16 +125,14 @@ class MapActivity : AppCompatActivity() {
         }
     }
 
-    private fun replaceBottomSheetContent(contentView: View) {
-        val bottomSheet = findViewById<FrameLayout>(R.id.bottom_sheet)
-        bottomSheet.removeAllViews()
-        bottomSheet.addView(contentView)
-
-        // Expand the bottom sheet
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    private fun replaceBottomSheetContent(fragment: Fragment) {
+        // Replace the content of the bottom sheet with the selected fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.bottom_sheet, fragment)
+            .commit()
     }
 
-      fun onMapItemSelected(selectedPosition: Int) {
+    fun onMapItemSelected(selectedPosition: Int) {
         Log.d("MapActivity", "Item selected at position: $selectedPosition")
 
         // Determine which form type is selected
@@ -78,12 +144,17 @@ class MapActivity : AppCompatActivity() {
             else -> 1 // Default to the first form
         }
 
-        // Inflate the layout for the selected form
-        val formLayout = inflateFormLayout(selectedFormType)
+        // Determine which fragment is selected based on the form type
+        val selectedFragment = when (selectedFormType) {
+            1 -> Form1Fragment()
+            2 -> Form2Fragment()
+            3 -> Form3Fragment()
+            4 -> Form4Fragment()
+            else -> Form1Fragment()
+        }
 
-        // Replace the content of the bottom sheet with the selected form layout
-        replaceBottomSheetContent(formLayout)
+        replaceBottomSheetContent(selectedFragment)
 
-        showBottomSheet()
+        hideBottomSheet()
     }
 }
